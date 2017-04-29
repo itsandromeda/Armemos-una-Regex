70b7cb9f68168542319c092b5fb58989e6ebb11e
itsandromeda <itsandromeda@gmail.com>
2017-04-28 21:09:01 -0500

    Form design

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,55 +30,55 @@ function validateForm() {
         checkbox = document.getElementById("terms");
     //Name validation
     if (firstChar.test(name.value) === false) {
-        document.getElementById("name-error").style.display = "inline-block";
+        document.getElementById("name-error").style.opacity = "1";
     } else {
-        document.getElementById("name-error").style.display = "none";
+        document.getElementById("name-error").style.opacity = "0";
     }
     //Last name validation
     if (firstChar.test(lastname.value) === false) {
-        document.getElementById("lastname-error").style.display = "inline-block";
+        document.getElementById("lastname-error").style.opacity = "1";
     } else {
-        document.getElementById("lastname-error").style.display = "none";
+        document.getElementById("lastname-error").style.opacity = "0";
     }
     //ID validation
     if (idRegex.test(idNumber.value) === false) {
-        document.getElementById("dni-error").style.display = "inline-block";
+        document.getElementById("dni-error").style.opacity = "1";
     } else {
-        document.getElementById("dni-error").style.display = "none";
+        document.getElementById("dni-error").style.opacity = "0";
     }
     //Cel number validation
     if (celRegex.test(celNumber.value) === false) {
-        document.getElementById("cel-error").style.display = "inline-block";
+        document.getElementById("cel-error").style.opacity = "1";
     } else {
-        document.getElementById("cel-error").style.display = "none";
+        document.getElementById("cel-error").style.opacity = "0";
     }
     //Home number validation
     if (homeRegex.test(homeNumber.value) === false) {
-        document.getElementById("home-error").style.display = "inline-block";
+        document.getElementById("home-error").style.opacity = "1";
     } else {
-        document.getElementById("home-error").style.display = "none";
+        document.getElementById("home-error").style.opacity = "0";
     }
     //Password validation
     if (password.value !== confirmation.value) {
-        document.getElementById("password-error-a").style.display = "inline-block";
-        document.getElementById("password-error-b").style.display = "none";
+        document.getElementById("password-error-a").style.opacity = "1";
+        document.getElementById("password-error-b").style.opacity = "0";
     } else if (password.value === "") {
-        document.getElementById("password-error-b").style.display = "inline-block";
-        document.getElementById("password-error-a").style.display = "none";
+        document.getElementById("password-error-b").style.opacity = "1";
+        document.getElementById("password-error-a").style.opacity = "0";
     } else {
-        document.getElementById("password-error-a").style.display = "none";
-        document.getElementById("password-error-b").style.display = "none";
+        document.getElementById("password-error-a").style.opacity = "0";
+        document.getElementById("password-error-b").style.opacity = "0";
     }
     //Comments validation
     if (comments.value === "") {
-        document.getElementById("comments-error").style.display = "inline-block";
+        document.getElementById("comments-error").style.opacity = "1";
     } else {
-        document.getElementById("comments-error").style.display = "none";
+        document.getElementById("comments-error").style.opacity = "0";
     }
     //Checkbox validation
     if (checkbox.checked === false) {
-        document.getElementById("checkbox-error").style.display = "inline-block";
+        document.getElementById("checkbox-error").style.opacity = "1";
     } else {
-        document.getElementById("checkbox-error").style.display = "none";
+        document.getElementById("checkbox-error").style.opacity = "0";
     }
 }
